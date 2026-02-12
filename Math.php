6c6ea956df49b9e969d0558efd61084957871b9c
Heather Hudgeon <hhudgeon@gmail.com>
2026-02-12 11:40:56 -0600

    Revert "added divide method to math thingy"
    This reverts commit 8e6f8bafc2fde670fd00a645fb0376f31895647b.
    
    removing brays commit

--- Math.php
+++ Math.php
@@ -51,16 +51,6 @@ class Math {
         return $sum;
     }
 
-    /**
-     * @param $num1
-     * @param $num2
-     * @return float|string
-     */
-    public static function divide($num1, $num2){
-        $result = $num1 / $num2;
-        return $result;
-    }
-
 
 
     public static function pythagorean($num1, $num2){
